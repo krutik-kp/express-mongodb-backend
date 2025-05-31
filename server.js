2d9ecf408c406ea7a40fef55679d60d77a3fdadf
krutik.dev <krutik5436@gmail.com>
2025-05-31 15:24:35 +0530

    changes for apis setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authApis =  require('./routes/auth')
+const categoryApis = require('./routes/categories')
+const productsApis = require('./routes/products')
 
 dotenv.config();
 const app = express();
@@ -10,16 +13,16 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
-app.use('/api/login', require('./routes/auth'));
+app.use('/api/login', authApis);
 
-app.use('/api/categories', require('./routes/categories'));
-app.use('/api/products', require('./routes/products'));
+app.use('/api/categories', categoryApis);
+app.use('/api/products', productsApis);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
